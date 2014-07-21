07989a9f986fa4da64e8fbf6e9c762df43930e6b
Jacob <jakeboone@aol.com>
2014-07-21 16:31:35 -0600

    completed part2

--- colorutil.js
+++ colorutil.js
@@ -1,10 +1,13 @@
+
 module.exports = {
+	 myColor: function(){
+		var r = process.argv[2];
+		var g = process.argv[3];
+		var b = process.argv[4];
 
-	var r = process.argv[2];
-	var g = process.argv[3];
-	var b = process.argv[4];
+		var luminosity = 0.2126*r + 0.7152*g + 0.0722*b
 
-	var luminosity = 0.2126*r + 0.7152*g + 0.0722*b
-
-	console.log(luminosity) 
+		return luminosity
+		// console.log(luminosity) 
+	}
 };
--- luminosity-cli.js
+++ luminosity-cli.js
@@ -2,14 +2,6 @@
 
 var colorutil = require('./colorutil.js')
 
-var colorNum = colorutil(process.argv[2], process.argv[3], process.argv[4]) 
 
 
-
-
-
-
-// var colorNum = colorutil
-
-
-console.log(colorNum)
+console.log(colorutil.myColor())
