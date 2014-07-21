4cecb2f0086a71bdb615b96302580966894f6d99
Jacob <jakeboone@aol.com>
2014-07-21 17:56:20 -0600

    changed darkin to a string commented out array

--- colorutil.js
+++ colorutil.js
@@ -12,7 +12,8 @@ module.exports = {
 	darken: function(r,g,b){
 
 
-		var darkLumin = [0.8*r, 0.8*g, 0.8*b]
+		// var darkLumin = [0.8*r, 0.8*g, 0.8*b]
+		var darkLumin = 0.8*r + " " + 0.8*g + " " + 0.8*b
 
 		return darkLumin
 
